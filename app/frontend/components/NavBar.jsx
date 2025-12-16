e30eb4a676a36b8946637eb98cffae22f7c0c45b
Valentin Bracchetti <valentin.bctt@gmail.com>
2025-12-16 15:17:03 +0100

    add log out

--- app/frontend/components/NavBar.jsx
+++ app/frontend/components/NavBar.jsx
@@ -3,8 +3,24 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHouse } from "@fortawesome/free-regular-svg-icons";
 import { faClockRotateLeft } from "@fortawesome/free-solid-svg-icons";
 import { faComments } from "@fortawesome/free-regular-svg-icons";
+import { faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
+
 
 export default function NavBar() {
+  const handleLogout = async () => {
+  try {
+    await fetch("/logout", {
+      method: "DELETE",
+      credentials: "include",
+    });
+
+    localStorage.removeItem("ai_last_auth");
+    window.location.href = "/";
+  } catch (err) {
+    console.error("Erreur logout", err);
+  }
+};
+
   return (
     <>
       {/* NAVBAR DESKTOP (verticale) */}
@@ -41,6 +57,17 @@ export default function NavBar() {
             Feedback
           </span>
         </a>
+                <button
+  onClick={handleLogout}
+  className="relative group w-12 h-12 rounded-lg transition-colors duration-200 hover:bg-white hover:text-[#cabb90] flex items-center justify-center text-2xl mt-auto mb-6"
+  aria-label="Déconnexion"
+>
+  <FontAwesomeIcon icon={faRightFromBracket} />
+  <span className="pointer-events-none absolute left-1/2 top-full mt-2 -translate-x-1/2 whitespace-nowrap rounded px-2 py-1 text-xs font-medium text-white opacity-0 transform scale-95 transition-all duration-150 group-hover:opacity-100 group-hover:scale-100">
+    Déconnexion
+  </span>
+</button>
+
       </nav>
 
       {/* NAVBAR MOBILE (horizontale en bas) */}
@@ -72,6 +99,8 @@ export default function NavBar() {
             <FontAwesomeIcon icon={faComments} />
           </div>
         </a>
+
+
 
       </nav>
     </>

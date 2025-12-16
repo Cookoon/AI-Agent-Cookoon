2f1d601676412ed48651b61cbc53139f4f3c1b81
Valentin Bracchetti <valentin.bctt@gmail.com>
2025-12-16 14:52:53 +0100

    Fix VITE_API_URL prod

--- app/frontend/components/Identification.jsx
+++ app/frontend/components/Identification.jsx
@@ -31,7 +31,7 @@ const handleSubmit = async () => {
 
 
 
-  const res = await fetch(`/api/login`, {
+  const res = await fetch(`/login`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     credentials: "include",
